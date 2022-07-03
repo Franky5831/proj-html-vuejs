53b1e1337bd5583d9b0b9bf5e4e0ebdb8c37b02a
Franky5831 <fracianna5@gmail.com>
2022-07-04 00:56:10 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: '/proj-html-vuejs',
   publicPath: process.env.NODE_ENV === 'production' ? '/' : './'
 })
 
